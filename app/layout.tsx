188fc17318d0296725602774e272c95ff297d610
Gunjan Sharma <gunjanatrish32@gmail.com>
2025-07-11 19:56:23 +0530

    auth0 create

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
 import "./globals.css";
 
 const geistSans = Geist({
